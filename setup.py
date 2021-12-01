fe08b3d5fa984f4276b4ed403b800ebfc8f6c50b
huoyo <1729913829@qq.com>
2021-12-01 22:45:31 +0800

    add include_package_data param in setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@
 @Author:   Chang Zhang
 @Date  :   2021/12/1 21:11
 '''
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
+
 setup(
-    name = 'ndraw',
-    version = '1.0.0',
-    packages = find_packages(),
-    author = 'Chang Zhang',
-    author_email = '1729913829@qq.com',
-    url = 'https://gitee.com/huoyo/ndraw',
+    name='ndraw',
+    version='1.0.1',
+    packages=find_packages(),
+    author='Chang Zhang',
+    author_email='1729913829@qq.com',
+    url='https://gitee.com/huoyo/ndraw',
     license="Apache License",
-    description = 'a tool to show neural networks',
-    data_files = [
-        ('core',['core/MetricFlow.js','core/template.html']),]
+    include_package_data=True,
+    description='a tool to show neural networks',
+    data_files=[
+        ('core', ['core/MetricFlow.js', 'core/template.html']), ]
 )
