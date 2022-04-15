5ab0d00be4646ad0f1b4225c8fd791229c6d30bb
huoyo <1729913829@qq.com>
2022-04-15 23:54:40 +0800

    update flow

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ndraw',
-    version='1.1.1',
+    version='1.1.2',#未发布
     packages=find_packages(),
     author='Chang Zhang',
     author_email='1729913829@qq.com',
--- testAuto.py
+++ testAuto.py
@@ -10,13 +10,13 @@ g = ndraw.AutoGraph(name_unique=False)
 #       ndraw.Node("第二2",data=["name:测试","color:hggg","color:hggg"]),
 #       ndraw.Node("第二1",data=["name:测试","color:hggg","color:hggg"])
 #       ])\
-start1= g.create("开始")
+start1= g.create("开始").to("开始2").to("开始3").to("开始4")
 start2= start1.to("开始2")
 start3= start1.to("开始3")
 start3_= start1.to("开始31")
 nodes4= start2.to("中间")
 nodes4= start3.to("中间")
-nodes4_= start3_.to("中间")
+# nodes4_= start3_.to("中间")
 
 
 # 多类型创建  任意关系创建
